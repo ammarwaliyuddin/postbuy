0da35f9b5c7e0cbbafe9468cc16a9734c7a98941
Ammar Waliyuddin Jannah <ammar.jannah@mncgroup.com>
2022-06-10 10:32:05 +0700

    first commit

--- api.py
+++ api.py
@@ -1,37 +1,45 @@
 
 from flask import Flask, jsonify, request, make_response
+import base64
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+def base64_decode(data):
+  base64_string = data
+  base64_bytes = base64_string.encode("ascii")
 
-@app.route("/")
-def home_view():
-        return "<h1>Welcome to Geeks for Geeks</h1>"
+  sample_string_bytes = base64.b64decode(base64_bytes)
+  sample_string = sample_string_bytes.decode("ascii")
+  return sample_string
+  
+
+# @app.route("/")
+# def home_view():
+#         return "<h1>Welcome to Geeks for Geeks</h1>"
         
 @app.route('/karyawan', methods=['GET','POST'])
 def karyawan():
     if request.method == 'POST':
         datainput = request.json
         print(datainput)
-        nama = datainput['nama']
-        pekerjaan = datainput['pekerjaan']
-        # type_order = datainput['typer_order']
-
+        
+        advertiser = datainput['advertiser']
+        year = datainput['year']
+        month = datainput['month']
+        no_po = base64_decode(datainput['no_po'])
+        type_order = datainput['type_order']
         # Fatih kerja bagian ini
-
-
-
-
-
 
 
         # end fatih ngerjain 
         data = {
-            'pesan': 'Data Berhasil Terkirim!!!',
-            'nama':nama
-            # 'pekerjaan':pekerjaan,
-            # 'usia':usia 
+            'pesan': True,
+            'advertiser' : advertiser,
+            'year' :year,
+            'month' :month,
+            'no_po' :no_po,
+            'type_order' :type_order
         }
         
         return make_response(jsonify({'data':data}),200)
